3a9bcd10348e83a7f6d8d47ecfd0cc37e6b5eb54
Brian Brown Coopman <brianbrowncoopman@gmail.com>
2023-04-21 23:03:51 -0400

    2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://Brianbrowncoopman.github.io/vite-react-rick-morty/",
   plugins: [react()],
 });
